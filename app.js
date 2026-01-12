3edbca4609036fcfb7d5744ce5b8a1cf890e6f75
mrchopzy-ui <mr.chopzy@gmail.com>
2026-01-12 17:01:38 +0530

    upload

---
+++ app.js
@@ -0,0 +1,34 @@
+const API_KEY = "63ed2d47625c4c03b27aaf0c8b99d7cf";
+
+const stocks = [
+  "RELIANCE.NSE","HDFCBANK.NSE","WIPRO.NSE","NESTLEIND.NSE","KOTAKBANK.NSE",
+  "ADANIPOWER.NSE","ITC.NSE","ONGC.NSE","HINDALCO.NSE","GAIL.NSE"
+];
+
+async function getQuote(symbol) {
+  const url = `https://api.twelvedata.com/quote?symbol=${symbol}&apikey=${API_KEY}`;
+  const res = await fetch(url);
+  return res.json();
+}
+
+async function loadData() {
+  const tbody = document.getElementById("tableBody");
+  tbody.innerHTML = "";
+
+  for (let sym of stocks) {
+    const q = await getQuote(sym);
+    if (!q.price) continue;
+
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+      <td>${sym.replace(".NSE","")}</td>
+      <td>${q.price}</td>
+      <td>${q.percent_change}%</td>
+      <td>${q.high}</td>
+    `;
+    tbody.appendChild(tr);
+  }
+}
+
+loadData();
+setInterval(loadData, 15000);
