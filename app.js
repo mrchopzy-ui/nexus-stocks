58a44fd519f58b035ab500e3323df2a254058a28
mrchopzy-ui <mr.chopzy@gmail.com>
2026-01-12 16:20:28 +0530

    fix api key

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_KEY = "d5ib7h9r01qmmfjfr51gd5ib7h9r01qmmfjfr520";
+const API_KEY = d5id2f9r01qmmfjg5l30d5id2f9r01qmmfjg5l3g;
 
 const stocks = [
   "RELIANCE.NS","HDFCBANK.NS","WIPRO.NS","NESTLEIND.NS","KOTAKBANK.NS",
